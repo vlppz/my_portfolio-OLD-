bd151a9a8579642b8700a7a0aee50a3c2c815e6c
Vladimir Papazov <papazovvladimir88@gmail.com>
2023-02-03 22:10:33 +0300

    sdfafasdfsadfa

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,9 @@ function Home() {
     return (
         <main className='pxl-10 pt-5'>
             <div className='w-screen flex justify-center items-center'>
-                <div className="text-7xl p-4 bg-purple-200 rounded-xl transition-all ease-in-out duration-500 hover:rotate-12"><span className="cursor-default font-appleemoji">✋🏻</span></div>
+                <div className="text-7xl p-4 bg-purple-200 rounded-xl transition-all ease-in-out duration-500 hover:rotate-12">
+                    <img src="./hand.png" alt="hand" className="h-20" />
+                </div>
             </div>
             <h1 className="font-light text-4xl w-screen text-center mt-4">Hello, my name is Vladimir!</h1>
         </main>
